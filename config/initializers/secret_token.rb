61901119aa2704e25a47d7ec12b1fddc1646daa3
Philipp Muens <raute1337@gmx.de>
2012-10-28 20:35:23 +0100

    Initial Commit. Added additional unneeded files in the .gitignore-file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collablearn::Application.config.secret_token = '50973339f84a1ce95e21b4ef1b2f4e905a3a24ead6324d83ce80820283a1464e7a02deb82ef86a83e793111ddb59ae5de181f670be1404858de50d12976325a8'
