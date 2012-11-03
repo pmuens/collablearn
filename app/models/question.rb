0a98c52da6c020253a1ce4b164e91c886f5308b0
Philipp Muens <raute1337@gmx.de>
2012-11-03 18:36:19 +0100

    - Added the Gem "acts_as_taggable" - Added configs for the "acts_as_taggable_gem" - Added the german locale - Switched Timezone to "Berlin" - Switched the default language to german - A Question now has a tag_list

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,9 +1,12 @@
 class Question < ActiveRecord::Base
   belongs_to :user
 
-  attr_accessible :answer, :question, :title
+  attr_accessible :answer, :question, :title, :tag_list
+
+  acts_as_taggable
 
   validates_presence_of :title
   validates_presence_of :question
   validates_presence_of :answer
+  validates_presence_of :tag_list
 end
