23ac3513f7cc847dcda501178710ee0ed27da8c9
Philipp Muens <raute1337@gmx.de>
2012-11-12 22:17:41 +0100

    - Added validation for the "collection" and "question"-model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -7,7 +7,7 @@ class Collection < ActiveRecord::Base
   acts_as_taggable_on :tags
   acts_as_followable
 
-  validates_presence_of :name
-  validates_presence_of :description
+  validates :name, presence: true, length: { in: 1..50 }
+  validates :description, presence: true, length: { in 1..300 }
   validates_presence_of :tag_list
 end
